41609b18e821accc74bafbb0d5bce721193f7ff3
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2023-04-11 22:41:22 -0300

    clase fecha

---
+++ POO/fechas/fechas/Fecha.cpp
@@ -0,0 +1,121 @@
+#include "Fecha.h"
+#include <iostream>
+
+using namespace std;
+
+///ERROR EN EL CONSTRUCTOR
+
+//constructores
+Fecha::Fecha(int dia = 1, int mes = 1, int anio = 2023) {
+	int canDias = cantidadDiaMes(mes, anio);
+	if (anio > 0 && mes >= 1 && mes <= 12 && dia >= 1 && dia <= canDias) {
+		setAnio(anio);
+		setMes(mes);
+		setDia(dia);
+	}
+	else {
+		setAnio(2023);
+		setMes(1);
+		setDia(1);		
+	}
+}
+
+Fecha::Fecha() {
+	setDia(1);
+	setMes(1);
+	setAnio(2023);
+}
+
+//setters
+void Fecha::setDia(int numD) {
+	if (numD >= 1 && numD <= cantidadDiaMes(_mes, _anio)) {
+		_dia = numD;
+	}
+	else {
+		cout << "ingrese un dia valido." << endl;
+	}
+}
+
+void Fecha::setMes(int numM) {
+	if (numM >= 1 && numM <= 12) {
+		_mes = numM;
+	}
+}
+
+void Fecha::setAnio(int numA) {
+	if (numA >= 1) {
+		_anio = numA;
+	}
+}
+
+//getters
+int Fecha::getDia() {
+	return _dia;
+}
+
+int Fecha::getMes() {
+	return _mes;
+}
+
+int Fecha::getAnio() {
+	return _anio;
+}
+
+//Funciones de clase
+bool Fecha::anioBisiesto(int numAnio) {
+	if (numAnio % 4 == 0 && numAnio % 100 != 0 && numAnio % 400 == 0) {
+		return true;
+	}
+	return false;
+}
+
+int Fecha::cantidadDiaMes(int numMes, int numAnio) {
+	int canDias = 0;
+
+	switch (numMes) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			canDias = 31;
+			break;
+
+		case 2:
+			if (anioBisiesto(numAnio)) {
+				canDias = 29;
+			}
+			else {
+				canDias = 28;
+			}
+			break;
+
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			canDias = 30;
+			break;
+	}
+	cout << "Can ->" << numMes << endl << " -> a " << numAnio ;
+	return canDias;
+}
+
+void Fecha::agregarDia() {
+	if (_dia < cantidadDiaMes(_mes, _anio)) {
+		_dia++;
+	}
+	else {
+		_dia = 01;
+
+		if (_mes != 12) {
+			_mes++;
+		}
+		else {
+			_mes = 01;
+			_anio++;
+		}
+	}
+}
---
+++ POO/fechas/fechas/fechas.cpp
@@ -0,0 +1,22 @@
+// fechas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+#include "Fecha.h";
+int main()
+{
+    Fecha fech;
+
+    cout << "Dia: " << fech.getDia() << endl;
+    cout << "Mes: " << fech.getMes() << endl;
+    cout << "Anio: " << fech.getAnio() << endl;
+    
+
+    cout << "Hello World!\n";
+    cout << endl;
+
+
+
+}
